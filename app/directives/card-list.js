ceccb0ab39eb6d9daa3a27c66c26a5ea7ddaccb0
dotan talitman <dotan talitman>
2016-09-27 16:50:20 +0300

    add count param to card-list diretive

--- app/directives/card-list.js
+++ app/directives/card-list.js
@@ -5,10 +5,51 @@
 
     function cardList() {
         return {
-            template: `<li ng-repeat="i in [0,1,2,3]">
-                     <card number='{{ $index+1 }}'></card>
-                      </li>`
+            $scope: {
+                count: "@"
+            },
+            template: template,
+            controller: controller,
+            link: link,
+            compile: compile
+
         }
+
+
+        function template(elem, attrs) {
+            return `<li ng-repeat="i in createArray(${attrs.count})  track by $index">
+                      <card number='{{$index+1}}'></card>
+                    </li>`
+        }
+
+        function compile(tElem, tAttrs) {
+            return {
+                pre: function (scope, iElem, iAttrs, controller) {
+                  
+                },
+                post: function (scope, iElem, iAttrs, controller) {
+                   
+                }
+            }
+
+        };
+        function link(scope, element, attr) {
+            console.log("scope", scope)
+            console.log("element", element)
+            console.log("attr", attr)
+
+        };
+
+
+        function controller($scope, $element, $attrs, $transclude) {
+
+            $scope.createArray = function (num) {
+                console.log(num)
+                return new Array(num)
+            }
+
+        }
+
     }
 })();
 
